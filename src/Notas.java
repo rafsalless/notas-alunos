53b0a69f2b64194350d58111e2221a4d0d350f36
Rafsalless <rafaelasallescomer@gmail.com>
2023-09-14 17:37:35 -0300

    first commit

---
+++ src/Notas.java
@@ -0,0 +1,11 @@
+public class Notas {
+    
+    public static void main(String[] args) {
+        
+        int nota = 6;
+
+        String resultado = nota >= 6 ? "aprovado" : "reprovado";
+
+        System.out.println(resultado);
+    }
+}
